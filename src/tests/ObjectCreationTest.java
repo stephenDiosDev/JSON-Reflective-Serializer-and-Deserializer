9273d0ad716d3cff5661119a80d58c4f956aba32
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-11-11 15:23:31 -0700

    Added multiple object1 test

--- src/tests/ObjectCreationTest.java
+++ src/tests/ObjectCreationTest.java
@@ -29,6 +29,23 @@ public class ObjectCreationTest {
         assertEquals("[a: 22] [b: 69.9] [c: true]\n", send.toString());
     }
 
+    @Test
+    public void testObject1MultipleObject() {
+        String data = "create object1\n" + "22\n" + "69.9\n" + "true\n" +
+                      "create object1\n" + "13\n" + "102.76\n" + "false\n" +
+                      "create object1\n" + "-2076\n" + "-2.76\n" + "false\n" +
+                      "create object1\n" + "0\n" + "0.0\n" + "true\n" + "send\n";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        send.driver();
+
+        assertEquals("[a: 22] [b: 69.9] [c: true]\n" +
+                "[a: 13] [b: 102.76] [c: false]\n" +
+                "[a: -2076] [b: -2.76] [c: false]\n" +
+                "[a: 0] [b: 0.0] [c: true]\n", send.toString());
+    }
+
     //reverts system.out to normal and allows us to access whats stored in baos
     public static void cleanup() {
 

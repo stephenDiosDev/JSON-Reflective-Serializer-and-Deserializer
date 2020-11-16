420baa2c3253bfbed071a5199c426b077d0d320a
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-11-15 19:17:25 -0700

    Started implementing JSON object creation

--- src/program/Serializer.java
+++ src/program/Serializer.java
@@ -1,13 +1,47 @@
 package program;
 
+import java.util.IdentityHashMap;
+
 public class Serializer {
     /* TODO
     Create json object and store the object
-
      */
-    public static String serializeObject(Object source) {
-        String result = "";
 
+    public static String serializeObject(Object source) {
+        IdentityHashMap hashMap = new IdentityHashMap();
+        int depth = 1;
+        String result = "{\n\t\"objects\": [";
+
+        result += jsonObject(source, depth + 1, hashMap);
+
+
+
+
+
+
+
+        return result;
+    }
+
+    private static String tabs(int depth) {
+        String tabs = "";
+
+        for(int i = 0; i < depth; i++) {
+            tabs += "\t";
+        }
+
+        return tabs;
+    }
+
+    private static String jsonObject(Object src, int depth, IdentityHashMap hashMap) {
+        String tabs = tabs(depth);
+        String result = tabs + "{\n";
+        result += tabs + "\t" +;
+
+
+
+
+        result += tabs + "},\n";
         return result;
     }
 

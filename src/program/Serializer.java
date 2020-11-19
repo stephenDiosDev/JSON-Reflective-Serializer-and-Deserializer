f30fe2c466ea884a952e6175dac3351f1f511787
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-11-18 21:46:26 -0700

    Started rework

--- src/program/Serializer.java
+++ src/program/Serializer.java
@@ -4,6 +4,7 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.IdentityHashMap;
 import javax.json.*;
 
@@ -12,91 +13,17 @@ public class Serializer {
     Create json object and store the object
      */
 
-    public static String serializeObject(Object source) {
-        IdentityHashMap hashMap = new IdentityHashMap();
-        int depth = 1;
-        String result = "";
-
-        //turn this into a loop for all objects so it doesnt print the above multiple times
-        result += jsonObject(source, depth + 1, hashMap);
-
-        return result;
+    public static String serializeObject(Object source, IdentityHashMap hashMap, ArrayList<String> jsonStrings) {
+        return jsonObject(source, hashMap, jsonStrings);
     }
 
-    private static String tabs(int depth) {
-        String tabs = "";
-
-        for(int i = 0; i < depth; i++) {
-            tabs += "\t";
+    private static String jsonObject(Object src, IdentityHashMap hashMap, ArrayList<String> jsonStrings) {
+        if(hashMap.containsValue(src.hashCode())) {
+            //json object and json stringwriter setup and start writing stuff in
         }
-
-        return tabs;
-    }
-
-    private static String jsonObject(Object src, int depth, IdentityHashMap hashMap) {
-        String tabs = tabs(depth);
-        String result = "\n" + tabs + "{\n";
-
-        JsonObject jsonObject;
-        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-        jsonObjectBuilder.add("class", src.getClass().getName());
-        jsonObjectBuilder.add("id", 0);
-
-        
-        StringWriter stringWriter = new StringWriter();
-        JsonWriter jsonWriter = Json.createWriter(stringWriter);
-
-        jsonWriter.writeObject(jsonObject.set);
-
-        //do class, id, type, fields
-        String tabInfo = tabs + "\t";
-        result += tabInfo;  //setup new tab amount
-
-        //id check
-        if(!hashMap.containsValue(src.hashCode())) {
-            //class name
-            result += "\"class\": \"" + src.getClass().getName() + "\",\n";
-
-            result += tabInfo;
-
-            int id = hashMap.size();
-            hashMap.put(id, src.hashCode());
-            result += "\"id\": \"" + id + "\",\n";
-
-
-            if(src.getClass().isArray()) {
-                String type = "array";
-            }
-            else {
-                String type = "object";
-
-                result += tabInfo + "\"type\": \"" + type + "\",\n";
-                result += tabInfo + "\"fields\": [\n";
-
-                //fields call
-                Field[] fields = src.getClass().getDeclaredFields();
-
-                for(int i = 0; i < fields.length; i++) {
-                    result += tabInfo + "\t{\n";
-                    result += jsonField(src, fields[i], depth + 1);
-                    result += tabInfo + "\t}";
-                    if(i != fields.length - 1)  //last element dont put comma
-                        result += ",";
-                    result += "\n";
-                }
-            }
-
-
-            result += tabInfo + "]\n";
-
-
-
-
+        else {  //duplicate
+            return "";
         }
-
-
-        result += tabs + "}\n";
-        return result;
     }
 
     private static String jsonField(Object source, Field field, int depth) {
